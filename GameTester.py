87d6adfe4e57e97ca22a828872d0ae4c5fc2020b
Reese Withouthisspoon <yungunz03@gmail.com>
2018-07-25 20:45:23 +0000

    More game progress, TES:Prequel

--- GameTester.py
+++ GameTester.py
@@ -5,12 +5,36 @@ prob = random.random()
 loading_screen = random.randrange(0,5)
 loading_screen2 = random.randrange(0,5)
 loading_screen3 = random.randrange(0,5)
-
+#variables
+health = 100
+print ("A message from the Developer:")
+time.sleep(3)
+print '''Hi, thanks for playing my game, most likely you'll
+be seeing my game while it's still very early and far from complete'''
+(time.sleep(3.5))
+print "So thank you for helping me out by testing it and giving me your input"
+time.sleep(4)
+print "P.S. To those of you seeing this at camp, I'm looking forward to your feedback, it's been great learning with you all!"
+print "This is one of my most ambitious projects and I'd love for it to become a legit game"
+print "Follow my instagram if you want updates @simply_reese03"
+time.sleep(8)
 def skyrim():
+    print "Launching skyrim_80.exe"
     print "Initializing data"
     time.sleep(1)
-    print "downloading fun"
+    print "installing fun.exe"
+    time.sleep(2)
+    print "executing fun.exe"
     time.sleep(1)
+    print "loading graphics"
+    time.sleep(3)
+    print "G@F!C$ F@!iUR3 3J3CT!NG"
+    time.sleep(5)
+    print "error, graphics attempt failed"
+    time.sleep(1)
+    print "continue loading skyrim_80.exe"
+    time.sleep(2)
+    print "Initializing world"
     if loading_screen == 0:
         print "creating goblins"
     if loading_screen == 1:
@@ -23,47 +47,64 @@ def skyrim():
         print "creating quests"
     if loading_screen == 5:
         print "Look out for Easter Eggs"
-    time.sleep(1)
+    time.sleep(1.3)
     if loading_screen2 == 0:
         print "forging swords"
     if loading_screen2 == 1:
         print "casting spells"
     if loading_screen2 == 2:
-        print ""
+        print "Fighting giants"
     if loading_screen2 == 3: 
-        print "finding artifacts"
+        print "summoning daedra"
     if loading_screen2 == 4:
-        print "creating quests"
+        print "pillaging villages"
     if loading_screen2 == 5:
-        print "Look out for Easter Eggs"
-    time.sleep(1)
+        print "Not being sued"
+    time.sleep(1.7)
     if loading_screen3 == 0:
-        print "creating goblins"
+        print "still not being sued"
     if loading_screen3 == 1:
-        print "Learning spells"
+        print "Learning Shouts"
     if loading_screen3 == 2:
-        print "fighting skeletons"
+        print "Fus Roh Dah"
     if loading_screen3 == 3: 
-        print "finding artifacts"
+        print "Climbing Mountains"
     if loading_screen3 == 4:
-        print "creating quests"
+        print "Delving into dungeons"
     if loading_screen3 == 5:
         print "Look out for Easter Eggs"
-    print ("A message from the Developer:")
-    print ('''Hi thanks for playing my game, most likely you'll be seeing 
-        my game while it's still very early and far from complete ''')  
-    name = raw_input("What's your name?")
-    if name == "Reese":
-        print ("Welcome Home")
-    else:
-        print ("hi", name)
+    time.sleep(2.5)
+    print "loading complete, launching game"
     time.sleep(1)
-    age = raw_input("How old are you?")
-    print ("Oh", age, "thats an exciting year isn't it?")
+    print ""
+    name = raw_input("What's your name? ")
+    if name == "Reese":
+        print ("Return of the King")
+    elif name == "tester":
+        print ("Thanks for testing my game! (The usual line is welcome ____, the blank being a name that isn't secret, I have easter eggs hidden, this name is one of them to the world of Skyrim")    
+    elif name == "dovahkiin":
+        print "FUS RO DAH"
+    elif name == "silence my brother":
+        print "Welcome home"
+    elif name == "sanguine my brother":
+        print "Welcome home"
+    elif name == "innocence my brother":
+        print "Welcome home"
+    else:
+        print ("Welcome", name, "to the world of Skyrim")
+    time.sleep(1)
+    print ''' ten years now, it's been ten years
+    since I lost my parents. Killed by bandits the townsfold said,
+    but I know it isn't true. I saw them with my own eyes. Demon like creatures.
+    I've read about them, now I know what they are, daedra. '''
 while True:
-    start = raw_input("press X to start")
+    start = raw_input("press X to start  ")
     if start == "x":
         skyrim()
+    elif start == "fus ro dah":
+        print "Sweet! You found a bronze easter egg, good job. This one was easy, there are still more"
+    elif start == "sweet mother, sweet mother, send your child unto me, for the sins of the unworthy must be baptized in blood and fear":
+        print "By Sithis, you've discovered a silver Easter Egg, and Audience/others who read this, don't be alarmed, I'm not a murderer, thats a quote from the Dark Brotherhood, a band of assassins in Skyrim"
     else:
         print "press X to start"
-    start = raw_input("press X to start")
+    start = raw_input("press X to start  ")
